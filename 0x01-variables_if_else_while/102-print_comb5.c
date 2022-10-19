db257ca34cf8df74b0689366fcf634ed1955339c
tush <wafulaat@gmail.com>
2022-10-19 11:41:03 -0700

    comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,29 +10,38 @@
 
 int main(void)
 {
-	int p, q;
+	int i, j, k, l;
 
-	for (p = 0; p <= 98; p++)
+	i = 48;
+	while (i < 58)
 	{
-		for (q = p + 1; q <= 99; q++)
+		j = 48;
+		while (j < 58)
 		{
-			putchar((p / 10) + '0');
-			putchar((p % 10) + '0');
-			putchar(' ');
-			putchar((q / 10) + '0');
-			putchar((q % 10) + '0');
-
-			if (p == 98 && q == 99)
-				continue;
-
-			putchar(',');
-			putchar(' ');
+			k = i;
+			while (k < 58)
+			{
+				if (i == k)
+					l = j + 1;
+				else 
+					l = 48;
+				while (l < 58)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i == 57 && j == 56 && k == 57 && l == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+					l++;
+				}
+				k++;
+			}
+			j++;
 		}
-	}
-
-	putchar('\n');
-
-	return (0);
+		putchar('\n');
+		return (0);
 }
-Footer
-
