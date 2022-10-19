784a55e0ab0a0b4022a52b14653f6b795d9b2b0a
tush <wafulaat@gmail.com>
2022-10-19 11:06:34 -0700

    alphabet

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 
 /**
  * main - Entry point
@@ -10,11 +9,11 @@
 
 int main(void)
 {
-	char low;
+	char c;
 
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
-
+	for (c = 'z'; c >= 'a'; c--){
+		putchar(c);
+	}
+	putchar('\n');
 	return (0);
 }
