d9e703f4912c816ac0c077e0845c84718e42340c
tush <wafulaat@gmail.com>
2022-10-19 10:50:05 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,16 +10,12 @@
 
 int main(void)
 {
-	char low, e, q;
-
-	e = 'e';
-	q = 'q';
+	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
-	{
-		if (low != e && low != q)
-			putchar(low);
-	}
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
 	putchar('\n');
 
 	return (0);
